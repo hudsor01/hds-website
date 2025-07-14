607b83a23d76e6657dbe2c33cdcfaf7cce2f4175
hudsor01 <hudsor01@icloud.com>
2025-07-14 02:05:25 -0500

    added nav

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@
 </template>
 
 <script setup lang="ts">
-import { Navbar } from './components/layout/Navbar.vue'
-import { Footer } from './components/layout/Footer.vue'
+import Navbar from './components/layout/Navbar.vue'
+import Footer from './components/layout/Footer.vue'
 </script>
 
 <style>
